28fd92412bbe99265bba965fd6e3a5a0c7cc581c
欧易 OKX 官方网站 <dy2025061901@163.com>
2025-06-23 12:23:09 +0800

    Create okx.java
    update

---
+++ okx.java
@@ -0,0 +1,18 @@
+// okx.java
+// 易欧交易所app官方下载-交易所官网入口
+// https://www.bestokx.com/
+import java.io.*;
+import java.okx.*;
+public class OKX {
+    public static void main(String[] args) throws IOException {
+        char c;
+        // 使用 System.in 创建 BufferedReader
+        BufferedReader okx = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("okcoin交易平台app下载-okcoin官方安卓app|版本2025");
+        // 读取字符
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        } while (c != 'okx');
+    }
+}
